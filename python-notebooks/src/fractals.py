1a65d08c4f3f6f733792a51032386e15fa2ab29f
jfjhh <alex.striff1@gmail.com>
2020-05-31 16:54:39 -0400

    Information dimension

--- python-notebooks/src/fractals.py
+++ python-notebooks/src/fractals.py
@@ -8,6 +8,7 @@ import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter, ImageOps
 from scipy import interpolate
+from scipy import integrate
 from src.intensity_entropy import *
 from src.kernels import *
 plt.rcParams['image.cmap'] = 'inferno'
@@ -64,33 +65,58 @@ boxdim(dots)
 
 # ## Information dimension
 
-# Figured out problem: discretized samples must still be normalized. I.e. integrate over $\varepsilon$-segment to produce value. See [Wikipedia information dimension](https://en.wikipedia.org/wiki/Information_dimension#Connection_to_Differential_Entropy).
+def discretize(f, a, b, ε, N=20):
+    return [integrate.simps(f(np.linspace(c - ε/2, c + ε/2, N)), dx=ε / (N - 1))
+            for c in np.arange(a + ε/2, b, ε)]
 
-def infodim(dist):
-    spl = interpolate.splrep(range(len(dist)), dist, s=0) # s=2e-5
-    εs = np.arange(5, len(dist) - 1, 5)
-    logεs = np.log2(εs)
+
+def infodim(dist, s=1e-5):
+    l = len(dist)
+    spl = interpolate.splrep(range(l), dist, s=s)
+    f = lambda x: interpolate.splev(x, spl)
+    
+    εs = l / np.linspace(10, l)
+    logεs = -np.log2(εs)
     endεs = logεs[[0, -1]]
-    entropies = [shannon_entropy(
-        interpolate.splev(np.arange(0, len(dist), ε), spl)) for ε in εs]
-    dimfit = np.polyfit(np.log2(εs), entropies, 1) # [slope, intercept]
+    entropies = [shannon_entropy(discretize(f, 0, l, ε)) for ε in εs]
+    dimfit, cov = np.polyfit(logεs, entropies, 1, cov='unscaled')
+    
     plt.plot(endεs, dimfit[0]*endεs + dimfit[1])
     plt.plot(logεs, entropies, '+')
     plt.xlabel('Scale (lg ε)')
     plt.ylabel('Shannon entropy (bits)')
-    return dimfit
+    
+    return dimfit[0], cov[0,0]
 
 
-dist = (10 / 256) * np.exp(-np.linspace(-5, 5, 256)**2 / 2) / np.sqrt(2*np.pi)
+# The Gaussian distribution
+# $$
+# f(x)
+# = \frac{1}{\sigma\sqrt{2\pi}}\exp\left(-\frac{{(x - \mu)}^2}{2\sigma^2}\right)
+# $$
+# is continuous, so its information dimension is 1.
+
+def gaussian(μ, σ, x):
+    return np.exp(-(x - μ)**2 / (2*σ**2)) / (σ*np.sqrt(2*np.pi))
+
+
+infodim((10/256) * gaussian(0, 1, np.linspace(-5, 5, 256)))
+
+
+# The rectified Gaussian distribution $g(x) = \Theta(x)f(x) + \delta(x)/2$ is half-continuous, so its information dimension is 1/2.
+
+dist = np.concatenate([[0]*256, (5/256)*gaussian(0, 1, np.linspace(0, 5, 256))])
 plt.plot(dist);
 
 
 infodim(dist)
 
 
+# Now that we've validated `infodim`, what does it say about the intensity distribution of an image?
+
 dist = intensity_distribution(img)
 plt.plot(dist);
 
 
-plt.plot(np.sort(data.flatten()))
+infodim(dist)
 
